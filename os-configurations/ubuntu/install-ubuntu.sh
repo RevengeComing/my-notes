efd119b51bfe89318a25e1de79940c2c10e1aa2b
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2021-01-18 02:42:58 +0330

    First

---
+++ os-configurations/ubuntu/install-ubuntu.sh
@@ -0,0 +1,26 @@
+# install system requirements
+sudo apt install vim curl git wget
+
+# install oh-my-zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install python
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.7-dev python3.8-dev python3.9-dev python3.6-dev python3.5-dev python2.7-dev virtualenv
+
+# install golang
+wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -P /tmp
+sudo ar -C /usr/local -xzf /tmp/go1.15.6.linux-amd64.tar.gz
+
+# install docker CE
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
