846db1f65e948f9cd4bd07836d6123606f6286de
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2021-04-18 16:50:40 +0430

    Update install-ubuntu.sh

--- os-configurations/ubuntu/install-ubuntu.sh
+++ os-configurations/ubuntu/install-ubuntu.sh
@@ -12,7 +12,7 @@ sudo apt-get install python3.7-dev python3.8-dev python3.9-dev python3.6-dev pyt
 
 # install golang
 wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -P /tmp
-sudo ar -C /usr/local -xzf /tmp/go1.15.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go1.15.6.linux-amd64.tar.gz
 
 # install docker CE
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
